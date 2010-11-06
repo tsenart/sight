d7fd0305810847bd3c4d1974fb85db40be689b26
Tomás Senart <tsenart@me.com>
2010-11-06 02:44:15 +0000

    CSS loaded dynamically. v0.6.3

--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,7 @@
 var filename = document.location.pathname.split("/").pop().toLowerCase();
-var extension = filename.split(".").pop()
+var extension = filename.split(".").pop();
 var pres = document.getElementsByTagName("pre");
-if(filename && pres.length > 0 && document.body.firstChild == pres[0]) { 
+if(filename && pres.length > 0 && document.body.firstChild == pres[0]) {
     var table = [
       "bison", ["ypp", "y++", "y"],
       "c", ["c", "h"],
@@ -20,26 +20,36 @@ if(filename && pres.length > 0 && document.body.firstChild == pres[0]) {
       "ruby", ["rakefile", "gemfile", "rb"],
       "xml", ["xml", "atom", "rss"]
     ];
-    
+
     for (var e = table.length - 1; e >= 0; e -= 2) {
         if (table[e].some(function(g) { return g == filename || g == extension })) {
             var lang = table[e - 1];
-            break
+            break;
         }
     }
+
+    if(!document.getElementsByTagName('head')[0]) {
+        head = document.createElement('head');
+        document.getElementsByTagName('html')[0].insertBefore(head, document.getElementsByTagName('html')[0].getElementsByTagName('*')[0]);
+    }
+    else {
+        head = document.getElementsByTagName('head')[0];
+    }
+
     document.body.className = 'sh_sourceCode';
     document.body.firstChild.className = "sh_" + lang;
-    
+
+    var el = document.createElement('link');
+    el.href = chrome.extension.getURL( 'css/main.css');
+    el.type = "text/stylesheet"; el.rel = "stylesheet";
+    head.appendChild(el);
+
     chrome.extension.sendRequest({key: "theme"}, function(msg) {
         var theme = msg.value;
-        var el = document.createElement('link');
+        el = document.createElement('link');
         el.href = chrome.extension.getURL( 'css/sh_' + theme + '.css');
-        el.type = "text/stylesheet"; el.rel = "stylesheet"; el.async = false;
-        if(!document.getElementsByTagName("head")[0]) {				
-	        var head = document.createElement('head');
-	        document.getElementsByTagName('html')[0].insertBefore(head, document.getElementsByTagName('html')[0].getElementsByTagName('*')[0]);
-        }
-        document.getElementsByTagName("head")[0].appendChild(el);
+        el.type = "text/stylesheet"; el.rel = "stylesheet";
+        head.appendChild(el);
     });
     sh_highlightDocument();
 }
