5a0fcefc0ae77dddfe298a9b85630b6b27ad938e
Tomás Senart <tsenart@me.com>
2011-01-18 03:26:01 +0000

    Added support for more fonts. Added "-webkit-font-smoothing: antialiased;" Moved to Browser Action insted of PageAction
    Signed-off-by: Tomás Senart <tsenart@me.com>

--- js/background.js
+++ js/background.js
@@ -1,17 +1,44 @@
 if(!localStorage['theme'] || localStorage['theme'] == '') localStorage['theme'] = 'sunburst';
+if(!localStorage['font'] || localStorage['font'] == '') localStorage['font'] = 'Inconsolata';
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {    
     if(sender && sender.tab && sender.tab.selected == false) return;
      
+     
+    if(request.key == 'plaintext')
+    {
+        if(request.op == 'get') {   
+            chrome.tabs.getSelected(null, function(stab) {
+                stab = sender.tab || stab;
+            
+                if(!localStorage[stab.id])
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font'], plaintext: false});
+                sendResponse({value: JSON.parse(localStorage[stab.id]).plaintext});
+            });
+        }
+        else if(request.op == 'set') {
+            chrome.tabs.getSelected(null, function(stab) {
+                stab = sender.tab || stab;
+                
+                if(!localStorage[stab.id])
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font'], plaintext: false});
+                var tab = JSON.parse(localStorage[stab.id]);
+                tab.plaintext = request.value;
+                localStorage[stab.id] = JSON.stringify(tab);
+            });
+        }
+        
+    }
+    
     if(request.key == 'theme')
     {
         if(request.op == 'get') {
             chrome.tabs.getSelected(null, function(stab) {
                 stab = sender.tab || stab;
-                console.log(_.sprintf('Tab %d Get Theme', stab.id))   
+                console.log(_.sprintf('Tab %d Get theme', stab.id))   
                 
                 if(!localStorage[stab.id])
-                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme']});
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
                 sendResponse({value: JSON.parse(localStorage[stab.id]).theme});
             });
         }
@@ -21,7 +48,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
                 console.log(_.sprintf('Tab %d Set theme', stab.id))   
                 
                 if(!localStorage[stab.id])
-                    localStorage[stab.id] = JSON.stringify({language: '', theme: ''});
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
                 var tab = JSON.parse(localStorage[stab.id]);
                 tab.theme = request.value;
                 localStorage[stab.id] = JSON.stringify(tab);
@@ -30,6 +57,34 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
                         chrome.tabs.insertCSS(stab.id, {file: 'css/' + tab.theme + '.css'});
                     });
                 });
+            });
+        }
+
+    }
+    
+    if(request.key == 'font')
+    {
+        if(request.op == 'get') {
+            chrome.tabs.getSelected(null, function(stab) {
+                stab = sender.tab || stab;
+                console.log(_.sprintf('Tab %d Get font', stab.id))   
+                
+                if(!localStorage[stab.id])
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
+                sendResponse({value: JSON.parse(localStorage[stab.id]).font});
+            });
+        }
+        else if(request.op == 'set') {
+            chrome.tabs.getSelected(null, function(stab) {
+                stab = sender.tab || stab;
+                console.log(_.sprintf('Tab %d Set font', stab.id))   
+                
+                if(!localStorage[stab.id])
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
+                var tab = JSON.parse(localStorage[stab.id]);
+                tab.font = request.value;
+                localStorage[stab.id] = JSON.stringify(tab);
+                chrome.tabs.executeScript(stab.id, {code: "document.body.firstChild.firstChild.style.fontFamily = '" + tab.font + "';"});
             });
         }
 
@@ -43,7 +98,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
                 console.log(_.sprintf('Tab %d Get Language', stab.id))   
                 
                 if(!localStorage[stab.id])
-                    localStorage[stab.id] = JSON.stringify({language: '', theme: ''});
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
                 sendResponse({value: JSON.parse(localStorage[stab.id]).language});
             });
         }
@@ -53,12 +108,12 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
                 console.log(_.sprintf('Tab %d Set Language', stab.id))   
                 
                 if(!localStorage[stab.id])
-                    localStorage[stab.id] = JSON.stringify({language: '', theme: ''});
+                    localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
                 var tab = JSON.parse(localStorage[stab.id]);
                 tab.language = request.value;
                 localStorage[stab.id] = JSON.stringify(tab);
                 chrome.tabs.executeScript(stab.id, {file: 'js/languages/' + tab.language + '.js' }, function(){
-                    chrome.tabs.executeScript(stab.id, {code: _.sprintf("hljs.reHighlight(document.body.firstChild, '%s')", tab.language)});
+                    chrome.tabs.executeScript(stab.id, {code: _.sprintf("hljs.reHighlight(document.body.firstChild, '%s');document.body.firstChild.firstChild.style.fontFamily ='%s';", tab.language, tab.font)});
                 });
             });
         }
@@ -70,7 +125,7 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         chrome.tabs.getSelected(null, function(stab) {
             stab = sender.tab || stab;
             if(!localStorage[stab.id])
-                localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme']});
+                localStorage[stab.id] = JSON.stringify({language: '', theme: localStorage['theme'], font: localStorage['font']});
             var tab = JSON.parse(localStorage[stab.id]);
             console.log(_.sprintf('Tab %d Highlight', stab.id))
             chrome.tabs.executeScript(stab.id, {file: 'js/reset-styles.js'});
@@ -79,9 +134,8 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
                 chrome.tabs.executeScript(stab.id, {code: xhr.responseText}, function() {
                     if(request.language && request.language != '') {
                         chrome.tabs.executeScript(stab.id, {
-                            code: _.sprintf("hljs.tabReplace = '    ';chrome.extension.sendRequest({key:'language',op:'set',value:'%s'});chrome.extension.sendRequest({key:'theme',op:'set',value:'%s'});", request.language, tab.theme)
+                            code: _.sprintf("hljs.tabReplace = '    ';chrome.extension.sendRequest({key:'language',op:'set',value:'%s'});chrome.extension.sendRequest({key:'theme',op:'set',value:'%s'});chrome.extension.sendRequest({key:'font',op:'set',value:'%s'});chrome.extension.sendRequest({key:'plaintext',op:'set',value:true});", request.language, tab.theme, tab.font)
                         });
-                        chrome.pageAction.show(stab.id);
                     }
                 });
             
--- js/inject.js
+++ js/inject.js
@@ -1,7 +1,6 @@
 var filename = document.location.pathname.split("/").pop().toLowerCase();
 var extension = filename.split(".").pop().toLowerCase();
 var pres = document.getElementsByTagName("pre");
-
 if(filename && pres.length > 0 && document.body.firstChild == pres[0]) {
     if(!document.getElementsByTagName('head')[0]) {
         var head = document.createElement('head');
@@ -45,5 +44,9 @@ if(filename && pres.length > 0 && document.body.firstChild == pres[0]) {
         document.body.firstChild.innerHTML = JSON.stringify(JSON.parse(document.body.firstChild.innerHTML), null, 4);
     }
     document.body.firstChild.innerHTML = '<code>' + document.body.firstChild.innerHTML + '</code>';
+    chrome.extension.sendRequest({key: 'plaintext', op: 'set', value: document.body.firstChild == pres[0]});
     chrome.extension.sendRequest({op: 'highlight', language: lang});
+}
+else {
+    chrome.extension.sendRequest({key: 'plaintext', op: 'set', value: document.body.firstChild == pres[0]});
 }
