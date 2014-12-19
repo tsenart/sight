e12898598b8618f4c28a0708cdb881fa3e812db5
SnackCode <dylan.frankland@gmail.com>
2014-12-19 10:43:22 -0800

    Remove for loop
    There’s only one pre tag per page therefore there’s no need for a loop.

--- js/lib/linenumbers.js
+++ js/lib/linenumbers.js
@@ -1,12 +1,9 @@
 (function() {
-    var preEl = document.getElementsByTagName('pre'),
-        preElLength = preEl.length;
-    for (var i = 0; i < preElLength; i++) {
-        preEl[i].innerHTML = '<span id="linenumbers"></span>' + preEl[i].innerHTML + '<span class="clearfix"></span>';
-        var num = preEl[i].innerHTML.split(/\n/).length;
-        for (var j = 0; j < num; j++) {
-            var lineNum = preEl[i].getElementsByTagName('span')[0];
-            lineNum.innerHTML += '<span>' + (j + 1) + '</span>';
-        }
-    }
+  var preEl = document.getElementsByTagName('pre');
+  preEl.innerHTML = '<span id="linenumbers"></span>' + preEl.innerHTML + '<span class="clearfix"></span>';
+  var num = preEl.innerHTML.split(/\n/).length;
+  for (var j = 0; j < num; j++) {
+    var lineNum = preEl.getElementsByTagName('span')[0];
+    lineNum.innerHTML += '<span>' + (j + 1) + '</span>';
+  }
 })();
